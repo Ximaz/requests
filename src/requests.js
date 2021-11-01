2ebd4f0dbe0dcb328a5563bab0fc2bad651bdc1f
quatrecentquatre-404 <obvious.ly.dev@gmail.com>
2021-11-01 13:10:19 +0100

    Static class method transformation, handling new Requests JSON methods.

--- src/requests.js
+++ src/requests.js
@@ -1,7 +1,7 @@
 /**
- * @author Quatrecentquatre
+ * @author Quatrecentquatre-404
  * @name Requests
- * @description This is a modular class to perfom requests easier. It's an interface between the developer and XHRs.
+ * @description Requests is a browser-side JS module which acts like an interface between XHRs and the developer. It helps to make asynchronous requests rather than using it's default callbacks.
  */
 
 /**
@@ -10,16 +10,15 @@
  * @returns {object} Parsed headers
  */
 function _parseResponseHeaders(headers) {
-    if (!headers) return {};
+    if (!headers) return {}
     const lines = headers.split(/\r\n/g),
-        parsedHeaders = {};
+        parsedHeaders = {}
 
     for (const line of lines) {
-        const key = line.split(/:/g)[0];
-        if (key)
-            parsedHeaders[key] = line.split(/:/g).slice(1).join(':').trim();
+        const key = line.split(/:/g)[0]
+        if (key) parsedHeaders[key] = line.split(/:/g).slice(1).join(':').trim()
     }
-    return parsedHeaders;
+    return parsedHeaders
 }
 
 /**
@@ -27,7 +26,7 @@ function _parseResponseHeaders(headers) {
  * @param {string} method Request method
  * @param {string} url URL to fetch
  * @param {object} headers Request headers
- * @param {string|object} body Content to send, string or object, depends on Content-Type
+ * @param {string} body Content to send, string or object, depends on Content-Type
  * @returns {object} Response
  */
 function _default(method, url, headers = {}, body = {}) {
@@ -36,10 +35,10 @@ function _default(method, url, headers = {}, body = {}) {
             ? new XMLHttpRequest()
             : window.ActiveXObject
             ? new ActiveXObject('Microsoft.XMLHTTP')
-            : null;
+            : null
 
         if (r === null)
-            throw 'Unable to initilize XMLHttpRequest on this browser.';
+            throw 'Unable to initilize XMLHttpRequest on this browser.'
 
         r.onload = function () {
             const data = {
@@ -47,129 +46,191 @@ function _default(method, url, headers = {}, body = {}) {
                 body: r.responseText,
                 json: (() => {
                     try {
-                        return JSON.parse(r.responseText);
+                        return JSON.parse(r.responseText)
                     } catch (e) {
-                        return null;
+                        return null
                     }
                 })(),
                 headers: _parseResponseHeaders(r.getAllResponseHeaders()),
                 xhr: r,
-            };
-            if (199 < r.status < 300) return resolve(data);
-            else return reject(data);
-        };
+            }
+            if (199 < r.status < 300) return resolve(data)
+            else return reject(data)
+        }
 
-        r.open(method, url);
+        r.open(method, url)
 
-        for (const [key, value] of Object.entries(headers))
-            r.setRequestHeader(key.toLowerCase(), value);
+        for (const [key, value] of Object.entries(headers)) {
+            delete headers[key]
+            headers[key.toLowerCase()] = value
+            r.setRequestHeader(key.toLowerCase(), value)
+        }
 
-        r.send(body);
-    });
+        if (headers['content-type'] === 'application/json')
+            body = JSON.stringify(body)
+
+        r.send(body)
+    })
 }
 
 class Requests {
-    constructor() {
-        /**
-         * GET
-         * @param {string} url URL to fetch
-         * @param {object} headers Request headers
-         * @returns {object} Response
-         */
-        this.get = async function (url, headers) {
-            try {
-                return await _default('GET', url, headers);
-            } catch (e) {
-                throw e;
-            }
-        };
+    /**
+     * GET
+     * @param {string} url URL to fetch
+     * @param {object} headers Request headers
+     * @returns {object} Response
+     */
+    static async get(url, headers = {}) {
+        try {
+            return await _default('GET', url, headers)
+        } catch (e) {
+            throw e
+        }
+    }
 
-        /**
-         * HEAD
-         * @param {string} url URL to fetch
-         * @param {object} headers Request headers
-         * @returns {object} Response
-         */
-        this.head = async function (url, headers) {
-            try {
-                return await _default('HEAD', url, headers);
-            } catch (e) {
-                throw e;
-            }
-        };
+    /**
+     * GET JSON
+     * @param {string} url URL to fetch
+     * @param {object} headers Request headers
+     * @returns {object} JSON response
+     */
+    static async getJSON(url, headers = {}) {
+        try {
+            return (await _default('GET', url, headers)).json
+        } catch (e) {
+            throw e
+        }
+    }
 
-        /**
-         * DELETE
-         * @param {string} url URL to fetch
-         * @param {object} headers Request headers
-         * @returns {object} Response
-         */
-        this.delete = async function (url, headers) {
-            try {
-                return await _default('DELETE', url, headers);
-            } catch (e) {
-                throw e;
-            }
-        };
+    /**
+     * HEAD
+     * @param {string} url URL to fetch
+     * @param {object} headers Request headers
+     * @returns {object} Response
+     */
+    static async head(url, headers = {}) {
+        try {
+            return await _default('HEAD', url, headers)
+        } catch (e) {
+            throw e
+        }
+    }
 
-        /**
-         * POST
-         * @param {string} url URL to fetch
-         * @param {object} headers Request headers
-         * @param {string|object} body Content to send, string or object, depends on Content-Type header
-         * @returns {object} Response
-         */
-        this.post = async function (url, headers, body = {}) {
-            try {
-                return await _default('POST', url, headers, body);
-            } catch (e) {
-                throw e;
-            }
-        };
+    /**
+     * DELETE
+     * @param {string} url URL to fetch
+     * @param {object} headers Request headers
+     * @returns {object} Response
+     */
+    static async delete(url, headers = {}) {
+        try {
+            return await _default('DELETE', url, headers)
+        } catch (e) {
+            throw e
+        }
+    }
 
-        /**
-         * PATCH
-         * @param {string} url URL to fetch
-         * @param {object} headers Request headers
-         * @param {string|object} body Content to send, string or object, depends on Content-Type header
-         * @returns {object} Response
-         */
-        this.patch = async function (url, headers, body = {}) {
-            try {
-                return await _default('PATCH', url, headers, body);
-            } catch (e) {
-                throw e;
-            }
-        };
+    /**
+     * POST
+     * @param {string} url URL to fetch
+     * @param {object} headers Request headers
+     * @param {string} body Content to send, string or object, depends on Content-Type header
+     * @returns {object} Response
+     */
+    static async post(url, headers = {}, body = null) {
+        try {
+            return await _default('POST', url, headers, body)
+        } catch (e) {
+            throw e
+        }
+    }
 
-        /**
-         * PUT
-         * @param {string} url URL to fetch
-         * @param {object} headers Request headers
-         * @param {string|object} body Content to send, string or object, depends on Content-Type header
-         * @returns {object} Response
-         */
-        this.put = async function (url, headers, body = {}) {
-            try {
-                return await _default('PUT', url, headers, body);
-            } catch (e) {
-                throw e;
-            }
-        };
+    /**
+     * POST JSON
+     * @param {string} url URL to fetch
+     * @param {object} headers Request headers
+     * @param {object} body Content to send, string or object, depends on Content-Type header
+     * @returns {object} Response
+     */
+    static async postJSON(url, headers = {}, body = {}) {
+        return await this.post(url, {...headers, "content-type": "application/json"}, JSON.stringify(body))
+    }
 
-        /**
-         * OPTIONS
-         * @param {string} url URL to fetch
-         * @param {object} headers Request headers
-         * @param {string|object} body Content to send, string or object, depends on Content-Type header
-         * @returns {object} Response
-         */
-        this.options = async function (url, headers, body = {}) {
-            try {
-                return await _default('OPTIONS', url, headers, body);
-            } catch (e) {
-                throw e;
-            }
-        };
+    /**
+     * PATCH
+     * @param {string} url URL to fetch
+     * @param {object} headers Request headers
+     * @param {string} body Content to send, string or object, depends on Content-Type header
+     * @returns {object} Response
+     */
+    static async patch(url, headers = {}, body = null) {
+        try {
+            return await _default('PATCH', url, headers, body)
+        } catch (e) {
+            throw e
+        }
+    }
+
+    /**
+     * PATCH JSON
+     * @param {string} url URL to fetch
+     * @param {object} headers Request headers
+     * @param {object} body Content to send, string or object, depends on Content-Type header
+     * @returns {object} Response
+     */
+    static async patchJSON(url, headers = {}, body = {}) {
+        return await this.patch(url, {...headers, "content-type": "application/json"}, JSON.stringify(body))
+    }
+
+    /**
+     * PUT
+     * @param {string} url URL to fetch
+     * @param {object} headers Request headers
+     * @param {string} body Content to send, string or object, depends on Content-Type header
+     * @returns {object} Response
+     */
+    static async put(url, headers = {}, body = null) {
+        try {
+            return await _default('PUT', url, headers, body)
+        } catch (e) {
+            throw e
+        }
+    }
+
+    /**
+     * PUT JSON
+     * @param {string} url URL to fetch
+     * @param {object} headers Request headers
+     * @param {object} body Content to send, string or object, depends on Content-Type header
+     * @returns {object} Response
+     */
+    static async putJSON(url, headers = {}, body = {}) {
+        return await this.put(url, {...headers, "content-type": "application/json"}, JSON.stringify(body))
+    }
+
+    /**
+     * OPTIONS
+     * @param {string} url URL to fetch
+     * @param {object} headers Request headers
+     * @param {string} body Content to send, string or object, depends on Content-Type header
+     * @returns {object} Response
+     */
+    static async options(url, headers = {}, body = null) {
+        try {
+            return await _default('OPTIONS', url, headers, body)
+        } catch (e) {
+            throw e
+        }
+    }
+
+    /**
+     * OPTIONS JSON
+     * @param {string} url URL to fetch
+     * @param {object} headers Request headers
+     * @param {object} body Content to send, string or object, depends on Content-Type header
+     * @returns {object} Response
+     */
+    static async optionsJSON(url, headers = {}, body = {}) {
+        return await this.options(url, {...headers, "content-type": "application/json"}, JSON.stringify(body))
     }
 }
